2a4c2459b97aa36d301f35a1bebef8441376580a
Robert Tisa <97615555+Raygard001@users.noreply.github.com>
2022-01-19 22:36:50 +0200

    Valorant Page ready

--- assets/js/new3dAnimation.js
+++ assets/js/new3dAnimation.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    const showSlider = new Swiper('showcase-Slayders', {
-        loop: true,
-        slidersPerView: 3,
-        speed: 1800
-    })
+	const showSlider = new Swiper('.showcase-carousel', {
+		loop: true,
+		slidesPerView: 3,
+        speed: 1800,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+          },
+	})
 
     document.querySelectorAll('#showcase__video').playbackRate = 2
 })
